1fedf3d0dc72e79a4ce481878d90d1807e549d4f
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2024-04-19 19:12:03 +0530

    build: config project for production

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -21,6 +24,11 @@ main().catch((err) => console.log(err));
 async function main(){
   await mongoose.connect(mongoDB);
 }
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
 
 const sessionStore = MongoStore.create({mongoUrl: process.env.MONGO_URL, collectionName: 'sessions'})
 app.use(session({
@@ -43,6 +51,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(compression());
+app.use(helmet());
+app.use(limiter)
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
